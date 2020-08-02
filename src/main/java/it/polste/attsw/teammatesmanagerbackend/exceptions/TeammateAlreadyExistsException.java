41cc02783344d48314e47c845ed852c4a9831661
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-08-02 17:35:02 +0200

    solved some warnings

--- src/main/java/it/polste/attsw/teammatesmanagerbackend/exceptions/TeammateAlreadyExistsException.java
+++ src/main/java/it/polste/attsw/teammatesmanagerbackend/exceptions/TeammateAlreadyExistsException.java
@@ -2,6 +2,8 @@ package it.polste.attsw.teammatesmanagerbackend.exceptions;
 
 public class TeammateAlreadyExistsException extends RuntimeException {
 
+  private static final long serialVersionUID = 7423819945893466047L;
+
   public TeammateAlreadyExistsException(String message) {
     super(message);
   }
--- src/main/java/it/polste/attsw/teammatesmanagerbackend/exceptions/TeammateNotExistsException.java
+++ src/main/java/it/polste/attsw/teammatesmanagerbackend/exceptions/TeammateNotExistsException.java
@@ -2,6 +2,8 @@ package it.polste.attsw.teammatesmanagerbackend.exceptions;
 
 public class TeammateNotExistsException extends RuntimeException {
 
+  private static final long serialVersionUID = 8491889381204662759L;
+
   public TeammateNotExistsException(String message) {
     super(message);
   }
