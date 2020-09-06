a33f72ceba53c2962e55afd3048ba376971b4867
Stefano Vannucchi <stefano.vannucchi1@gmail.com>
2020-09-06 10:51:28 +0200

    added container removal after stopping application in execution locally added volumes mapping for database to persist data also after database container removal

--- run_locally.sh
+++ run_locally.sh
@@ -1,22 +1,26 @@
 echo "Starting Teammates Manager"
 
-CONTAINER_ID=`docker ps -f "name=teammates-manager-db" -q`
+CONTAINER_ID=$(docker ps -f "name=teammates-manager-db" -q)
 if [ -z "$CONTAINER_ID" ]
 then
   echo "Starting MySQL database"
-  cd src/main/docker
+  cd src/main/docker || exit
   docker-compose up -d
   cd ../../../
-  CONTAINER_ID=`docker ps -f "name=teammates-manager-db" -q`
+  CONTAINER_ID=$(docker ps -f "name=teammates-manager-db" -q)
 else
   echo "Database already running"
 fi
 
 echo "Getting container network settings"
-CONTAINER_IP=`docker inspect -f '{{ (index (index .NetworkSettings.Ports "3306/tcp") 0).HostIp }}' $CONTAINER_ID`
-CONTAINER_PORT=`docker inspect -f '{{ (index (index .NetworkSettings.Ports "3306/tcp") 0).HostPort }}' $CONTAINER_ID`
+CONTAINER_IP=$(docker inspect -f '{{ (index (index .NetworkSettings.Ports "3306/tcp") 0).HostIp }}' "$CONTAINER_ID")
+CONTAINER_PORT=$(docker inspect -f '{{ (index (index .NetworkSettings.Ports "3306/tcp") 0).HostPort }}' "$CONTAINER_ID")
 DB_URL='jdbc:mysql://attsw:attsw@'$CONTAINER_IP':'$CONTAINER_PORT'/teammates-manager'
 
 echo "Setting up Application"
 ./mvnw clean package -Pbuild-frontend
 java -Dspring.profiles.active=prod -Dspring.datasource.url="$DB_URL" -jar target/*.jar
+
+echo "Removing MySQL database container"
+cd src/main/docker || exit
+docker-compose down
