1da0befaa5090757ae8621cdb0f7f7785ee32a17
Pol52 <innocentipaolo1@gmail.com>
2020-09-07 15:49:05 +0200

    removed unused imports from repositories

--- src/main/java/it/polste/attsw/teammatesmanagerbackend/repositories/SkillRepository.java
+++ src/main/java/it/polste/attsw/teammatesmanagerbackend/repositories/SkillRepository.java
@@ -2,7 +2,6 @@ package it.polste.attsw.teammatesmanagerbackend.repositories;
 
 import it.polste.attsw.teammatesmanagerbackend.models.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
--- src/main/java/it/polste/attsw/teammatesmanagerbackend/repositories/TeammateRepository.java
+++ src/main/java/it/polste/attsw/teammatesmanagerbackend/repositories/TeammateRepository.java
@@ -2,7 +2,6 @@ package it.polste.attsw.teammatesmanagerbackend.repositories;
 
 import it.polste.attsw.teammatesmanagerbackend.models.Teammate;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface TeammateRepository extends JpaRepository<Teammate, Long> {
